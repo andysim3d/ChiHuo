85fe2a332755a9224cf77cb645fe52e9db9f821e
Pengfei Zhang <pzhang11@stevens.edu>
2014-03-29 19:23:43 -0400

    add Entities
    Signed-off-by: Pengfei Zhang <pzhang11@stevens.edu>

---
+++ Vladimir/src/edu/stevens/cs562/Parameters.java
@@ -0,0 +1,14 @@
+package edu.stevens.cs562;
+
+import java.util.ArrayList;
+
+public class Parameters {
+	private ArrayList<String> S;
+	private int thea;
+	private ArrayList<String> G;
+	private ArrayList<String> F;
+	private ArrayList<String> pList;
+	private ArrayList<String> Having;
+	
+	
+}
